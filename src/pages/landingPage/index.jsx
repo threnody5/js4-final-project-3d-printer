c7fd98c9bd7d9d32f08b1b87b08744b3b397d556
William <williamw15@protonmail.com>
2023-03-08 09:04:52 -0500

    push to repo

--- src/pages/landingPage/index.jsx
+++ src/pages/landingPage/index.jsx
@@ -2,12 +2,13 @@
 
 import styles from './styles.modules.scss';
 import { useSelector } from 'react-redux';
+// import landingPageHero from './../../assets/landing-page-hero.png';
 
 const LandingPage = () => {
   const theme = useSelector((state) => state.theme.value);
   return (
     <div theme={theme}>
-      <h3>Landing page works</h3>
+      <img src={'./../../assets/landing-page-hero.jpg'} />
     </div>
   );
 };
